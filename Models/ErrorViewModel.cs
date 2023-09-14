5a803adf6ec8a6c0e43f26186c478d5cffcdac4f
Mikhail Arjan Sandigan <mikhailsandigan6@gmail.com>
2023-09-14 08:16:17 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Edoc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
